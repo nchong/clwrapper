da58feb6aaf693b13e1730ffc2584b7ee9b13809
Nathan <nathan.chong@gmail.com>
2011-11-12 13:41:30 +0000

    CX1 submit script only uses minimum necessary LD_LIBRARY_PATH.

--- test/saxpy/submit.sh
+++ test/saxpy/submit.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
+#PBS -l walltime=1:00:00
+#PBS -l select=1:ncpus=1:mem=3800mb:ngpus=0
+
 module load intel-suite/11.1
 module load opencl/intel-1.5
 
 cd $PBS_O_WORKDIR
 CLWRAPPER_LIB_PATH=$PBS_O_WORKDIR/../../lib
-export LD_LIBRARY_PATH=$CLWRAPPER_LIB_PATH/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$CLWRAPPER_LIB_PATH/:/apps/opencl/1.5/lib:/apps/opencl/1.5/lib/OpenCL/vendors/intel/:/apps/intel/cc/10.1.018/lib
 
 module list
 echo $LD_LIBRARY_PATH
