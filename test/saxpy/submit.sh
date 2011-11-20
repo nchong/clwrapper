08c22553ea1ac278354fd46e9cdbeb0aea1767af
Nathan <nathan.chong@gmail.com>
2011-11-20 23:41:11 +0000

    saxpy example uses platform 1 which is either amd/intel on CX1 depending on which is module loaded

--- test/saxpy/submit.sh
+++ test/saxpy/submit.sh
@@ -3,15 +3,24 @@
 #PBS -l select=1:ncpus=1:mem=3800mb:ngpus=0
 
 module load intel-suite/11.1
-module load opencl/intel-1.5
-
+module load opencl/amd-2.5
 cd $PBS_O_WORKDIR
 CLWRAPPER_LIB_PATH=$PBS_O_WORKDIR/../../lib
-export LD_LIBRARY_PATH=$CLWRAPPER_LIB_PATH/:/apps/opencl/1.5/lib:/apps/opencl/1.5/lib/OpenCL/vendors/intel/:/apps/intel/cc/10.1.018/lib
+export LD_LIBRARY_PATH=$CLWRAPPER_LIB_PATH:$LD_LIBRARY_PATH
 
+echo ">>>>> AMD-2.5"
 module list
 echo $LD_LIBRARY_PATH
 ldd $CLWRAPPER_LIB_PATH/libclwrapper.so
 ldd saxpy
+./saxpy
 
+module unload opencl/amd-2.5
+module load opencl/intel-1.5
+
+echo ">>>>> INTEL-2.5"
+module list
+echo $LD_LIBRARY_PATH
+ldd $CLWRAPPER_LIB_PATH/libclwrapper.so
+ldd saxpy
 ./saxpy
